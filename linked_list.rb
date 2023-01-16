8808f87016c0f34aa18993e770046a03e6840c79
Andre <aspbarruda@gmail.com>
2023-01-16 18:33:13 -0300

    Added size, head, tail and at functions

--- linked_list.rb
+++ linked_list.rb
@@ -1,23 +1,57 @@
 class LinkedList
-  attr_accessor :head
+  attr_accessor :head_node
   def initialize
-    @head = nil
+    @head_node = nil
+    @tail_node = nil
   end
 
   def append(value)
-    if @head == nil
-      @head = Node.new(value)
+    if @head_node == nil
+      @head_node = Node.new(value)
     else
-      tmp = @head
+      tmp = @head_node
       while tmp.next_node != nil
         tmp = tmp.next_node
       end
       tmp.next_node = Node.new(value)
+      @tail_node = tmp.next_node
     end
   end
 
-  def prependd(value)
-    @head = Node.new(value, @head)
+  def prepend(value)
+    @head_node = Node.new(value, @head_node)
+  end
+
+  def size
+    tmp = @head_node
+    size = 0
+    until tmp.nil?
+      tmp = tmp.next_node
+      size += 1
+    end
+    puts "There are #{size} nodes!"
+  end
+
+  def head
+    puts "The head of the linked list is #{@head_node.value}"
+  end
+
+  def tail
+    puts "The tail of the linked list is #{@tail_node.value}"
+  end
+
+  def at(index)
+    tmp = @head_node
+    i = 0
+    until i == index || tmp.nil?
+      tmp = tmp.next_node
+      i += 1
+    end
+    if tmp.nil?
+      puts "Value not present at this linked list!"
+    else
+      puts "Value at #{i} is #{tmp.value}"
+    end
   end
 end
 
@@ -32,5 +66,9 @@ end
 list = LinkedList.new
 list.append(3)
 list.append(2)
-list.prependd(5)
-puts list.head.value, list.head.next_node.value, list.head.next_node.next_node.value
+list.prepend(5)
+puts list.head_node.value, list.head_node.next_node.value, list.head_node.next_node.next_node.value
+list.size
+list.head
+list.tail
+list.at(0)
