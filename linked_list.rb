77b05ef977df51017f05d572e485b1f084dbbba0
Andre <aspbarruda@gmail.com>
2023-01-16 21:20:04 -0300

    Cosmetic changes

--- linked_list.rb
+++ linked_list.rb
@@ -1,18 +1,18 @@
+# Full linked list, with all nodes, including last link to nil
 class LinkedList
   attr_accessor :head_node
+
   def initialize
     @head_node = nil
     @tail_node = nil
   end
 
   def append(value)
-    if @head_node == nil
+    if @head_node.nil?
       @head_node = Node.new(value)
     else
       tmp = @head_node
-      while tmp.next_node != nil
-        tmp = tmp.next_node
-      end
+      tmp = tmp.next_node until tmp.next_node.nil?
       tmp.next_node = Node.new(value)
       @tail_node = tmp.next_node
     end
@@ -31,12 +31,12 @@ class LinkedList
     end
     puts "There are #{size} nodes!" if size > 1
     puts "There is #{size} node!" if size == 1
-    puts "There are no nodes!" if size == 0
+    puts 'There are no nodes!' if size.zero?
   end
 
   def head
     if @head_node.nil?
-      puts "There are no nodes!"
+      puts 'There are no nodes!'
       return
     end
     puts "The head of the linked list is #{@head_node.value}"
@@ -44,7 +44,7 @@ class LinkedList
 
   def tail
     if @head_node.nil?
-      puts "There are no nodes!"
+      puts 'There are no nodes!'
       return
     end
     puts "The tail of the linked list is #{@tail_node.value}"
@@ -58,15 +58,15 @@ class LinkedList
       i += 1
     end
     if tmp.nil?
-      puts "Index not present at this linked list!"
+      puts 'Index not present at this linked list!'
     else
       puts "Value at index (#{i}) is #{tmp.value}"
     end
   end
 
   def pop
-    if @head_node == nil
-      puts "Cannot pop element, list is empty"
+    if @head_node.nil?
+      puts 'Cannot pop element, list is empty'
       return
     elsif @head_node.next_node.nil?
       @head_node = nil
@@ -88,9 +88,10 @@ class LinkedList
     tmp = @head_node
     until tmp.nil?
       return true if tmp.value == value
+
       tmp = tmp.next_node
     end
-    return false
+    false
   end
 
   def find(value)
@@ -100,28 +101,29 @@ class LinkedList
     idx = 0
     until tmp.nil?
       return idx if tmp.value == value
+
       idx += 1
       tmp = tmp.next_node
     end
-    return nil
+    nil
   end
 
   def to_s
-    return "" if @head_node.nil?
+    return '' if @head_node.nil?
 
     tmp = @head_node
-    str = ""
+    str = ''
     until tmp.nil?
       str << "#{tmp.value} -> "
       tmp = tmp.next_node
     end
-    str << "nil"
+    str << 'nil'
     puts str
   end
 
   def insert_at(value, index)
-    if index == 0
-      self.prepend(value)
+    if index.zero?
+      prepend(value)
       return
     end
     cur = @head_node
@@ -134,15 +136,16 @@ class LinkedList
         return
       end
       break if tmp.nil?
+
       cur = tmp
       tmp = tmp.next_node
       idx += 1
     end
-    puts "Index greater than last index on this list"
+    puts 'Index greater than last index on this list'
   end
 
   def remove_at(index)
-    if index == 0
+    if index.zero?
       @head_node = @head_node.next_node
       return
     end
@@ -156,16 +159,19 @@ class LinkedList
         return
       end
       break if tmp.nil?
+
       cur = tmp
       tmp = tmp.next_node
       idx += 1
     end
-    puts "Index greater than last index on this list"
+    puts 'Index greater than last index on this list'
   end
 end
 
+# Created a new node on the linked list
 class Node
   attr_accessor :next_node, :value
+
   def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
